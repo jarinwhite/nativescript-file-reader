ea9fe09b7c01bf4f9da74384cdcf3f86ef6193b4
Jarin White <jarinwhite@gmail.com>
2016-12-31 22:59:21 -0600

    Create nativescript-file-reader.ts

---
+++ nativescript-file-reader.ts
@@ -0,0 +1,17 @@
+import * as fs from "file-system";
+let documents = fs.knownFolders.currentApp();
+
+export class FileReader {
+    public static readJSON(path: string): Promise<Object> {
+        let jsonFile = documents.getFile(path);
+        return new Promise<Object>((resolve, reject) => {
+            jsonFile.readText().then((content: string) => {
+                let data = <Array<Object>>JSON.parse(content);
+                resolve(data);
+            })
+            .catch((err) => {
+                reject(err);
+            });
+        });
+    }
+}
